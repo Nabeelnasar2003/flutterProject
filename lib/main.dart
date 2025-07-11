7b6721d8a81874642cbc29b689538fcd0ce8909c
muhammed nabeel nasar <muhammednabeelnasar0@gmail.com>
2025-07-12 00:36:26 +0530

    To Do List App

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,14 @@ void main() {
   runApp(const MainApp());
 }
 
+class Task {
+  String title;
+  bool isDone;
+
+  Task(this.title, {this.isDone = false});
+}
+
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
@@ -11,123 +19,102 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstScreen(),
+      home: ToDoList(),
       
     );
   }
   }
 
 
-final TextEditingController myController = TextEditingController();
-final TextEditingController nextController = TextEditingController();
+class ToDoList extends StatefulWidget {
+  const ToDoList({super.key});  
+
+  @override 
+  State<ToDoList> createState() => ToDoListState(); 
+}
+
+class ToDoListState extends State<ToDoList> {
+  final List<Task> task = [];
+  final TextEditingController taskController = TextEditingController();
 
 
 
-class SecondScreen extends StatelessWidget {
-  final String name;
-  final int age;
-  const SecondScreen({Key? key, required this.name , required this.age}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child:Column(
-mainAxisAlignment: MainAxisAlignment.center,
-children: [
-  Text(
-        '$name',
-  style: TextStyle(fontSize: 24, color: Colors.black)
-  ),
-  SizedBox(height: 20),
-  Text(
-        '$age',
-  style: TextStyle(fontSize: 24, color: Colors.black)
-  ),
-  SizedBox(height: 20),
-       ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Back'),
-        ),
-       ],
-      ),
-      ),
-    );
+  void addTask() {
+    if (taskController.text.isNotEmpty) {
+      setState(() {
+        task.add(Task(taskController.text));
+        taskController.clear();
+      });
+    }
   }
-} 
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({super.key});
+  void removeTask(int index) {
+    setState(() {
+      task.removeAt(index);
+    });
+  }
+
+
+
+
+void handleToDoChange(Task task) {
+  setState(() {
+    task.isDone = !task.isDone;
+  });
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-            padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: myController,
-              decoration: InputDecoration(
-                labelText: 'Enter name',
-                border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
-              ),
+      appBar: AppBar(title:  Text('To-Do List')),
+      body: Column(
+        children: [
+          Padding(
+            padding:  EdgeInsets.only(top: 20.0, left: 30.0, right: 30.0),
+            child: TextField(
+              controller: taskController,
+              decoration: const InputDecoration(labelText: 'Enter a task'),
+              style: const TextStyle(fontSize: 18.0, color: Colors.black),
+              
             ),
-            SizedBox(height:20),
-            TextField(
-              controller: nextController,
-              decoration: InputDecoration(
-                labelText: 'Enter Age',
-                border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
-              ),
-            ),
-            SizedBox(height: 15),
-        ElevatedButton(
-            onPressed: () {
-              if(myController.text.isEmpty) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text("Please enter name")),
-                );
-              }
-              else if(nextController.text.isEmpty) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text("Please enter age")),
-                );
-              }
-              else {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SecondScreen(
-                      name: myController.text,
-                      age: int.parse(nextController.text),
-                    ),
+          ),
+          SizedBox(height: 10.0),
+          ElevatedButton(onPressed:(){
+            if(taskController.text.isEmpty) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Please enter a task')),
+              );
+            } else {
+              addTask();
+            }
+          }, child: const Text('Add Task')),
+          SizedBox(height: 10.0),
+          
+          Padding(padding:  EdgeInsets.only(left: 30.0, right: 30.0),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: task.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(task[index].title,),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  tileColor: Colors.white,
+                  leading: Icon(
+                   task[index].isDone ? Icons.check_box : Icons.check_box_outline_blank,
+                    color: task[index].isDone ? Colors.green : Colors.grey,
+                  ),
+                  onTap: () => handleToDoChange(task[index]),            
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => removeTask(index),
                   ),
                 );
-              }
-            },
-          
-            child: const Text('Display'),
-          ),
-          SizedBox(height: 15),
-          ElevatedButton(
-            onPressed: () {
-              myController.clear();
-              nextController.clear();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text("Fields cleared")),
-              );
-            },
-            child: Text('Clear'),
+              },
+            ),
           ),
         ],
       ),
-    ),
-      )
-  );
+    );
   }
 }
